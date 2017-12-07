5f1d700587a907fbfa7778a8b3a162d2e8dda28f
Dion <dionvanderlinden@live.nl>
2017-12-07 16:51:26 +0100

    Route protocol aangemaakt, code formatting

--- Route.java
+++ Route.java
@@ -20,6 +20,16 @@ public class Route
         return controlCodes;
     }
 
+    /*
+     * Route protocol voorbeeld: >,0x66,0x66, 0x6c, 0x66, 0x72, 0x66, 0x00
+     * Vertaald: 0x3e > geeft begin aan van reeks, als dit ontbreekt is het geen geldig verzoek
+     * 0x66 FORWARD Voorwaards
+     * 0x6c LEFT Links
+     * 0x72 RIGHT Rechts
+     * 0x00 \0 Null terminator geeft eind van reeks aan
+     * @param data protocol
+     * @return Route object
+     */
     public static Route getRoute(ArrayList<Integer> data)
     {
         if(!(data.size() > 0)) return null;    
@@ -34,20 +44,20 @@ public class Route
             }
             switch(data.get(i))
             {
-                case 0x66: 
-                controlCodes.add(ControlCode.FORWARD);
+                case 0x66:
+                    controlCodes.add(ControlCode.FORWARD);
                 break;
                 case 0x6c:
-                controlCodes.add(ControlCode.LEFT);
+                    controlCodes.add(ControlCode.LEFT);
                 break;
                 case 0x72:
-                controlCodes.add(ControlCode.RIGHT);
+                    controlCodes.add(ControlCode.RIGHT);
                 break;
                 //case 0x73:
                 //controlCodes.add(ControlCode.STOP);
                 //break;
                 default:
-                return null;
+                    return null;
             }
         }
         return new Route(controlCodes);
