dad50638d62ff7c8356e9bf55d099de2f08334b2
Dion <dionvanderlinden@live.nl>
2017-12-08 16:20:55 +0100

    writeData functionaliteit toegevoegd

--- Bluetooth.java
+++ Bluetooth.java
@@ -8,26 +8,46 @@ public class Bluetooth
 {
     private final SerialConnection conn;
     private final int BAUDRATE = 115200;
-    private ArrayList<Integer> data;
+    private ArrayList<Integer> readData;
+    private Queue<Byte> writeQueue;
     
     public Bluetooth()
     {
         conn = new SerialConnection(BAUDRATE);
-        data = new ArrayList<>();
+        readData = new ArrayList<>();
+        writeQueue = new LinkedList<>();
     }
     
     public void update()
     {
-        if (conn.available() > 0) data.add(conn.readByte());            
-        else data.clear();
+        if (writeQueue.size() > 0) conn.writeByte(writeQueue.remove());
+        if (conn.available() > 0) readData.add(conn.readByte());            
+        else readData.clear();
     }
     
     public boolean dataReady()
     {
-        return (conn.available() == 0 && data.size() > 0);
+        return (conn.available() == 0 && readData.size() > 0);
     }
     public ArrayList<Integer> getData()
     {
-        return data;
+        return readData;
+    }
+    
+    public void writeData(byte[] data) // java reference?
+    {
+        // writeQueue = new LinkedList<>(Arrays.asList(data)); ?? waarom werkt dit niet
+        writeQueue.clear();
+        for (int i = 0; i < data.length; i++)
+        {
+            writeQueue.add(data[i]);
+        }
+
+        conn.writeByte(writeQueue.remove());
+    }
+    
+    public boolean isWriting()
+    {
+        return writeQueue.size() > 0;
     }
 }
